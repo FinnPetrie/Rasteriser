033eb7db58df6ed5842dc5318d4f83af3c3d452b
Finn Petrie <FinnPetrie@Finns-MBP-1.student.uod.otago.ac.nz>
2018-08-13 12:09:30 +1200

    added matrix multi-function

---
+++ Rasteriser/Camera.cpp
@@ -0,0 +1,9 @@
+//
+//  Camera.cpp
+//  Rasteriser
+//
+//  Created by Finn Petrie on 13/08/18.
+//  Copyright © 2018 Finn Petrie. All rights reserved.
+//
+
+#include "Camera.hpp"
---
+++ Rasteriser/Camera.hpp
@@ -0,0 +1,14 @@
+//
+//  Camera.hpp
+//  Rasteriser
+//
+//  Created by Finn Petrie on 13/08/18.
+//  Copyright © 2018 Finn Petrie. All rights reserved.
+//
+
+#ifndef Camera_hpp
+#define Camera_hpp
+
+#include <stdio.h>
+
+#endif /* Camera_hpp */
